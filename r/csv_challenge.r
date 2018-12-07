e4ff04bb6e7b750e86a558cc8c85f058401fe06c
Alishan Ladhani <alishan.ladhani@gmail.com>
2018-12-06 23:44:33 -0500

    Added R exercise

---
+++ r/csv_challenge.r
@@ -0,0 +1,7 @@
+library("jsonlite")
+
+frames <- fromJSON("https://gist.githubusercontent.com/jorinvo/7f19ce95a9a842956358/raw/e319340c2f6691f9cc8d8cc57ed532b5093e3619/data.json")
+frames <- frames[!is.na(frames$creditcard),]
+frames <- frames[,c("name","creditcard")]
+
+write.csv(frames, file="20171112.csv", row.names=FALSE)
