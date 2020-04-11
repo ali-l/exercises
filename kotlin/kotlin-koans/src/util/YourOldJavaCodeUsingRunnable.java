b959c7454f0207e861d62fc5a000d7b057d16618
Alishan Ladhani <alishan.ladhani@gmail.com>
2020-04-11 13:18:11 -0400

    Clean up

--- kotlin/kotlin-koans/src/util/YourOldJavaCodeUsingRunnable.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public class YourOldJavaCodeUsingRunnable {
-    public static void run(Runnable runnable) {
-        runnable.run();
-    }
-}
--- kotlin/kotlin-koans/src/util/kotlinUtil.kt
+++
@@ -1,6 +0,0 @@
-package util
-
-@Suppress("UNUSED_PARAMETER")
-fun TODO(task: String, documentation: Unit = Unit, vararg references: Any?): Nothing = throw NotImplementedException(task)
-
-class NotImplementedException(message: String): Exception(message)
